b7fa76378354c9f8bbab29501e95840d38b0cbdf
Cristina <c.rgallego@alumnos.upm.es>
2019-10-20 11:53:07 +0200

    Versión funcionando con marcadores de cámara y ubicación actual. Se piden permisos al usuario y se obtiene la ubicación si no existe una actualizada Además, se integra la parte de pintar la ruta. Algunos comentarios añadidos

--- app/src/main/java/dte/masteriot/mdp/emergencies/MapsActivity.java
+++ app/src/main/java/dte/masteriot/mdp/emergencies/MapsActivity.java
@@ -288,7 +288,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
-    
+
     public void getCurrentLocation(){
         requestingLocationUpdates = true;
         fusedLocationClient.getLastLocation()
