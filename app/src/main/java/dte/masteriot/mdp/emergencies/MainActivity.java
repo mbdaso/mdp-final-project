fd91ecdf664658e594fa8db0dd8e4055d1fca8bd
Cristina <c.rgallego@alumnos.upm.es>
2019-10-20 11:40:40 +0200

    Versión funcionando con marcadores de cámara y ubicación actual. Se piden permisos al usuario y se obtiene la ubicación si no existe una actualizada Además, se integra la parte de pintar la ruta. Algunos comentarios añadidos

--- app/src/main/java/dte/masteriot/mdp/emergencies/MainActivity.java
+++ app/src/main/java/dte/masteriot/mdp/emergencies/MainActivity.java
@@ -32,18 +32,28 @@ public class MainActivity extends AppCompatActivity {
 
     //URL from which the list of cameras will be retrieved
     private static final String URL_CAMERAS = "http://informo.madrid.es/informo/tmadrid/CCTV.kml";
+
+    //TextView to show the number of emergencies
     private TextView text;
 
+    //ArrayLists containing the names, URLs and coordinates of the cameras
     ArrayList<String> nameURLS_ArrayList = new ArrayList<>();
     ArrayList<String> camerasURLS_ArrayList = new ArrayList<>();
     ArrayList<LatLng> coorURLS_ArrayList = new ArrayList<>();
 
+    //ListView that includes the list of cameras
     ListView lv;
+
+    //Object to parse the file retrieved from the cameras' URL
     XmlPullParserFactory parserFactory;
 
+    //ImageView to show either UPM MIOT logo or the selected camera image
     private ImageView im;
 
+    //Position in the list of the camera selected
     int pos=0;
+
+    //Number of emergencies (NO2 dangerous levels) detected
     int numEmergencies = 0;
 
     @Override
@@ -70,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         @SuppressWarnings( "deprecation" )
         protected String doInBackground(String... urls) {
             String response = "";
-
+            //Connects to the URL and parses the response to build the different ArrayLists with cameras' data
             HttpURLConnection urlConnection = null;
             try {
                 URL url = new URL( urls[0] );
@@ -93,14 +103,13 @@ public class MainActivity extends AppCompatActivity {
                                 cameraURL = cameraURL.substring(cameraURL.indexOf("http:"));
                                 cameraURL = cameraURL.substring(0, cameraURL.indexOf(".jpg") + 4);
                                 camerasURLS_ArrayList.add(cameraURL);
-                             //   response+=cameraURL + "\n";
+
                             } else if ("Data".equals(elementName)) {
                                 aux=parser.getAttributeValue(null,"name");
                                 if (aux.equals("Nombre")){
                                     String name;
                                     parser.nextTag();
                                     name=parser.nextText();
-                                    Log.v("aux1", name);
                                     nameURLS_ArrayList.add(name);
                                 }
 
@@ -125,21 +134,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
+            //It builds the adapter of the cameras' names list
             lv = (ListView) findViewById(R.id.lv);
             ArrayAdapter arrayAdapter = new ArrayAdapter( MainActivity.this, android.R.layout.simple_list_item_checked , nameURLS_ArrayList );
             lv.setAdapter(arrayAdapter);
-
             lv.setChoiceMode( ListView.CHOICE_MODE_SINGLE );
-
             lv.setClickable(true);
+            //It sets a listener on the list
             lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
                 @Override
                 public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
                     Object o = lv.getItemAtPosition(position);
                     String str=(String)o;//As you are using Default String Adapter
+                    //A toast with the name of the camera is shown
                     Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
                     pos=position;
+                    //It gets the image of the camera selected
                     ImageLoader task = new ImageLoader();
                     task.execute( camerasURLS_ArrayList.get(position) );
 
@@ -148,16 +159,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Async task class to retrieve the image of the camera from the Internet
     class ImageLoader extends AsyncTask<String, Void, Bitmap>{
-
-      //  ProgressDialog pDialog;
 
         @Override
         protected void onPreExecute() {
             // TODO Auto-generated method stub
             super.onPreExecute();
-
-
         }
 
         @Override
@@ -165,10 +173,9 @@ public class MainActivity extends AppCompatActivity {
             // TODO Auto-generated method stub
 
             String url = params[0];
-            //Bitmap imagen = descargarImagen(url);
-
             URL imageUrl = null;
             Bitmap imagen = null;
+            //It connects to the image url and gets the image
             try{
                 imageUrl = new URL(url);
                 HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
@@ -185,11 +192,14 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Bitmap result) {
             // TODO Auto-generated method stub
             super.onPostExecute(result);
+            //It sets the image to the image view to be shown to the User
             ImageView im = (ImageView)((AppCompatActivity) MainActivity.this).findViewById(R.id.imageView);
             im.setImageBitmap(result);
+            //It sets a listener on the image to load a map when clicked
             im.setOnClickListener( new View.OnClickListener(){
                 public void onClick(View v){
                     Intent intent = new Intent (v.getContext(), MapsActivity.class);
+                    //It passes the parameters needed in the new Maps activity
                     Bundle args = new Bundle();
                     args.putParcelable("coordinates", coorURLS_ArrayList.get(pos));
                     args.putString("cameraName", nameURLS_ArrayList.get(pos));
@@ -198,19 +208,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-     /*   private Bitmap descargarImagen (String imageHttpAddress){
-            URL imageUrl = null;
-            Bitmap imagen = null;
-            try{
-                imageUrl = new URL(imageHttpAddress);
-                HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
-                conn.connect();
-                imagen = BitmapFactory.decodeStream(conn.getInputStream());
-            }catch(IOException ex){
-                ex.printStackTrace();
-            }
-
-            return imagen;
-        }*/
     }
 }
--- app/src/main/java/dte/masteriot/mdp/emergencies/MapsActivity.java
+++ app/src/main/java/dte/masteriot/mdp/emergencies/MapsActivity.java
@@ -40,29 +40,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    //Map to be loaded in the activity
     private GoogleMap mMap;
+    //Positions of the camera selected and the current position of the user (if allowed)
     LatLng camPos, currPos;
+    //Name of the camera selected
     String cameraName;
+    //RadioButtons to change the map type
     RadioButton rbMap, rbSatellite, rbHybrid;
+    //Markers for camera position and user current position
     Marker camMarker, currPositionMarker;
+    //TAG to use in logs
     private String TAG = "MapsActivity";
-
+    //Variables for the different types of permissions requests
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
     private static final int PERMISSION_REQUEST_FINE_LOCATION = 2;
     private static final int PERMISSION_REQUEST_BOTH_LOCATION = 3;
-
-    // String permissionCoarseGranted, permissionFineGranted;
-
+    //Objects to handle the current location of the user
     private FusedLocationProviderClient fusedLocationClient;
-    LatLngBounds bounds;
     LocationRequest locationRequest;
     boolean requestingLocationUpdates;
     private LocationCallback locationCallback;
     Location mCurrentLocation;
 
+    //Bounds of the map with the markers
+    LatLngBounds bounds;
+    //Parameters received from previous activity
     Bundle args;
-
+    //Object of the route to be drawn
     private YOURSRoute yoursRoute = new YOURSRoute();
 
     @Override
@@ -86,12 +91,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (locationResult == null) {
                     return;
                 }
+                //It obtains last location of the user (if allowed)
                 mCurrentLocation = locationResult.getLastLocation();
+                //It sets current position marker, sets map bounds and draws the map route between the two points
                 currPos = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
                 addCurrPosMarker(currPos);
                 setMapBounds(currPos, camPos);
                 drawMapRoute(currPos, camPos);
-                fusedLocationClient.removeLocationUpdates(locationCallback);
+                //It stops receiving location updates
+                stopLocationUpdates();
+                //fusedLocationClient.removeLocationUpdates(locationCallback);
             }
 
 
@@ -110,6 +119,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onResume() {
         super.onResume();
+        //It retrieves location updates if the user allows it
         if (requestingLocationUpdates) {
             startLocationUpdates();
         }
@@ -123,8 +133,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -134,15 +143,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         //Before using Location services, location permissions are checked
         checkLocationPermissions();
-
+        //It loads RadioButtons on the map
         rbMap = (RadioButton) findViewById(R.id.rbMap);
         rbSatellite =(RadioButton) findViewById(R.id.rbSatellite);
         rbHybrid =(RadioButton) findViewById(R.id.rbHybrid);
+        //Hybrid type is set by default
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //It adds selected camera's position marker to the map
         camMarker = mMap.addMarker(new MarkerOptions().position(camPos).title(cameraName));
         camMarker.showInfoWindow();
     }
 
+    //It changes the map type to which is selected by the RadioButton clicked
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -151,21 +163,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         switch(view.getId()) {
             case R.id.rbMap:
                 if (checked) {
-                    Log.e(TAG, "RB Map");
+                  //  Log.e(TAG, "RB Map");
                     mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
                 }
                 break;
             case R.id.rbSatellite:
                 if (checked) {
-                    Log.e(TAG, "RB Satellite");
+                   // Log.e(TAG, "RB Satellite");
                     mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
                 }
                 break;
             case R.id.rbHybrid:
                 if (checked) {
-                    Log.e(TAG, "RB Hybrid");
+                   // Log.e(TAG, "RB Hybrid");
                     mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
                 }
                 break;
@@ -173,7 +185,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void checkLocationPermissions() {
-//If Android version is M (6.0 API 23) or newer, check if it has Location permissions to request them to the user in case it is needed
+        //If Android version is M (6.0 API 23) or newer, check if it has Location permissions to request them to the user in case it is needed
         /*
         Android offers two location permissions: ACCESS_COARSE_LOCATION and ACCESS_FINE_LOCATION. The permission you choose determines
         the accuracy of the location returned by the API. You only need to request one of the Android location permissions, depending on the level of accuracy you need:
@@ -182,7 +194,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         android.permission.ACCESS_FINE_LOCATION – Allows the API to determine as precise a location as possible from the available location providers,
         including the Global Positioning System (GPS) as well as WiFi and mobile cell data.
         */
-
+        //It requests the missing permissions to the user. In case both are granted, the app gets user's current location
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //Option 1: requesting both permissions
             if((this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
@@ -213,13 +225,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // permissionFineGranted = Manifest.permission.ACCESS_FINE_LOCATION;
         }*/
     }
+
+    //The response of the user invokes this method. It checks if the user either granted or denied the permissions
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        //Check if permission request response is from Location
+        //Check if permission request response is for location permissions
         int length = grantResults.length;
         switch (requestCode) {
             case PERMISSION_REQUEST_COARSE_LOCATION: {
                 if (length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //User granted permissions. Setup the scan settings
+                    //User granted permissions. It gets his/her current location
                     Log.i( TAG, "/+++++++++++++++++++++");
                     Log.d("TAG", "coarse location permission granted");
                     Log.i( TAG, "/+++++++++++++++++++++");
@@ -228,10 +242,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     getCurrentLocation();
 
                 } else {
-                    //User denied Location permissions. Here you could warn the user that without
-                    //Location permissions the app is not able to scan for BLE devices
-                    //In this case we just close the app
-                    //finish();
+                    //User denied Location permissions. His/her current location cannot be retrieved
                     requestingLocationUpdates = false;
                     Log.e(TAG, "User denied coarse location permissions");
                 }
@@ -240,7 +251,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             case PERMISSION_REQUEST_FINE_LOCATION:{
                 if (length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //User granted permissions. Setup the scan settings
+                    //User granted permissions. It gets his/her current location
+                    //It can be considered to warn the user
                     Log.i( TAG, "/+++++++++++++++++++++");
                     Log.d("TAG", "fine location permission granted");
                     Log.i( TAG, "/+++++++++++++++++++++");
@@ -249,10 +261,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     getCurrentLocation();
 
                 } else {
-                    //User denied Location permissions. Here you could warn the user that without
-                    //Location permissions the app is not able to scan for BLE devices
-                    //In this case we just close the app
-                    //finish();
+                    //User denied Location permissions. His/her current location cannot be retrieved
+                    //It can be considered to warn the user
                     requestingLocationUpdates = false;
                     Log.e(TAG, "User denied fine location permissions");
                 }
@@ -263,12 +273,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 if(length > 0){
                     for(int i = 0; i < grantResults.length ; i++){
+                        //If any of the permissions is denied, the location cannot be retrieved
                         if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
                             requestingLocationUpdates = false;
                             Log.e(TAG, "User denied some location permissions");
                             return;
                         }
                     }
+                    //In case both permissions are granted the location is retrieved
                     requestingLocationUpdates = true;
                     getCurrentLocation();
                 }
@@ -276,6 +288,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+    
     public void getCurrentLocation(){
         requestingLocationUpdates = true;
         fusedLocationClient.getLastLocation()
